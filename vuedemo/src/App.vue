447710dd84d89dc8df6881183b51e4fa904f1bbc
phpapi <phpapi@163.com>
2018-06-19 12:00:43 +0800

    vue

---
+++ vuedemo/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <router-view class="view" keep-alive transition transition-model="out-in">
+    </router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  comments: {}
+}
+</script>
+
+<style lang="scss">
+@import "./style/style";
+</style>
